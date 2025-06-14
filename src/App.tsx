4d13ba5a44961a8e4ada7599007a670863723b25
Jacob Goodall <jraygoodall@gmail.com>
2025-06-14 18:20:53 -0500

    adding to git

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { Counter } from "./components/Counter";
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world! github user search
+      </h1>
       <Counter />
     </div>
   );
